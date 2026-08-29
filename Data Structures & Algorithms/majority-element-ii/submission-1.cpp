2b20cf659e8f4bec6c9faac99e10e3a1d42ec9b5
Jenil Makwana <99378825+j-makwana@users.noreply.github.com>
2026-08-29 17:06:21 -0400

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+       vector<int> result;
+       unordered_map<int, int> temp;
+        int threshold = nums.size()/3;
+        int n = nums.size();
+        for(int i =0; i< n; i++){
+           if(temp[nums[i]]!=-1){
+            temp[nums[i]]++;
+           } 
+            if(temp[nums[i]]>threshold){
+                result.push_back(nums[i]);
+                temp[nums[i]]= -1;
+            }}
+        return result;
+
+    }
+};
